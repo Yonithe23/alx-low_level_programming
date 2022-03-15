9c0bfe68f2a491e3b4c6e6c2ded628b61387d8bd
Yonas Desta <yonasdesta2012@gmail.com>
2022-03-15 20:39:25 +0300

    correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,14 @@
  **/
 int main(void)
 {
-	char *s = “_putchar”
+	char yonas[] = "_putchar";
 
-while (*s != 0)
-{
-_putchar(*s);
-s++;
-}
-_putchar(“\n”);
-return (0);
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(yonas[c]);
+	}
+	_putchar('\n');
+	return (0);
 }
