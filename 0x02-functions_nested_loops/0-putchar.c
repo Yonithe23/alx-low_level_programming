64937be25fd40e373285eea5292bab1d417af223
Yonas Desta <yonasdesta2012@gmail.com>
2022-03-15 17:23:31 +0300

    add the last

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,15 @@
 
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	char str[] = "_putchar";
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		++i;
+	}
 	_putchar('\n');
 
 	return (0);
